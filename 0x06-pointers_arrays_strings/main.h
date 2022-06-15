1235efa784e0027167e237b3e755f7df1de5c87f
negelignmokon <negumoke59@gmail.com>
2022-06-15 12:27:34 -0700

    task12

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -14,4 +15,5 @@ int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 void print_buffer(char *b, int size);
+
 #endif /* MAIN_H */
