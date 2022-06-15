215e47555b9e61fb0f99c330cffc69ac7faa4c4a
negelignmokon <negumoke59@gmail.com>
2022-06-15 12:16:23 -0700

    hot fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-int _putchar(char);
+#include <stdio.h>
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,7 +10,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-void print_number(int n);
+int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
 void print_buffer(char *b, int size);
-#endif
+#endif /* MAIN_H */
