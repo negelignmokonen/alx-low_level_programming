8bc19acd066ea836dff0655fb7835149a8d26812
negelignmokon <negumoke59@gmail.com>
2022-06-16 11:59:06 -0700

    task10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -21,3 +21,14 @@ int main(void)
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
