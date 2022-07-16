0e4a73721d9c2d5cb556471271d9bd7600009c4e
negelignmokon <negumoke59@gmail.com>
2022-07-16 11:58:57 -0700

    hot fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,44 +1,48 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "main.h"
 /**
- * _realloc - ...
- * @ptr: ...
- * @old_size: ...
- * @new_size: ...
- *
- * Return: ...
+ * _realloc - reallocates old to new, set conditions from problem
+ * returning dest w/ size of malloc new_size, set src as ptr
+ * @ptr: pointer to memory prev alloc, must free end
+ * @old_size: input old
+ * @new_size: input new
+ * Return: 0
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *nptr;
+	char *dest, *src;
 	unsigned int i;
+
 	if (new_size == old_size)
 		return (ptr);
+
 	if (ptr == NULL)
 	{
-		nptr = malloc(new_size);
-		if (nptr == NULL)
-			return (NULL);
-		return (nptr);
-	}
-	else
-	{
-		if (new_size == 0)
+		ptr = malloc(new_size);
+		if (ptr == NULL)
 		{
-			free(ptr);
 			return (NULL);
 		}
+		return (ptr);
 	}
-	nptr = malloc(new_size);
-	if (nptr == NULL)
-		return (NULL);
-	for (i = 0; i < old_size && i < new_size; i++)
+
+	if (new_size == 0 && ptr != NULL)
 	{
-		nptr[i] = ((char *) ptr)[i];
+		free(ptr);
+		return (NULL);
 	}
+
+	dest = malloc(new_size);
+	if (dest == NULL)
+		return (NULL);
+
+	src = ptr;
+
+	for (i = 0; i < new_size && i < old_size; i++)
+		dest[i] = src[i];
 	free(ptr);
-	return (nptr);
+
+	return (dest);
 }
 
